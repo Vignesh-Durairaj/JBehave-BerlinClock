ceae287fc8e069678767e759d50acf8aee337ba1
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2018-08-02 00:24:04 +0530

    Changed the signature of the clock object

--- src/main/java/com/vikhi/practice/clock/BerlinClock.java
+++ src/main/java/com/vikhi/practice/clock/BerlinClock.java
@@ -1,47 +1,73 @@
 package com.vikhi.practice.clock;
 
-import java.util.Date;
+import java.util.stream.IntStream;
 
-public class BerlinClock implements Clock {
+import org.apache.commons.lang.StringUtils;
 
-	@Override
-	public int getHour(Date date) {
-		throw new UnsupportedOperationException("Yet to implement");
-	}
+import com.vikhi.practice.pojo.Light;
+import com.vikhi.practice.pojo.Time;
+import com.vikhi.practive.util.IConstants;
+import com.vikhi.practive.util.TimeUtil;
 
-	@Override
-	public int getMinute(Date date) {
-		throw new UnsupportedOperationException("Yet to implement");
-	}
+public class BerlinClock implements Clock, IConstants {
 
-	@Override
-	public int getSecond(Date date) {
-		throw new UnsupportedOperationException("Yet to implement");
-	}
-
-	@Override
-	public String getHourString(Date date) {
-		throw new UnsupportedOperationException("Yet to implement");
-	}
-
-	@Override
-	public String getMinuteString(Date date) {
-		throw new UnsupportedOperationException("Yet to implement");
-	}
-
-	@Override
-	public String getSecondString(Date date) {
-		throw new UnsupportedOperationException("Yet to implement");
-	}
-
+	private boolean isTurnedOn;
+	
+	private String secondsLight = StringUtils.EMPTY;
+	
+	private String firstRow = StringUtils.EMPTY;
+	
+	private String secondRow = StringUtils.EMPTY;
+	
+	private String thirdRow = StringUtils.EMPTY;
+	
+	private String fourthRow = StringUtils.EMPTY;
+	
 	@Override
 	public void turnClockOn() {
-		throw new UnsupportedOperationException("Yet to implement");
+		this.isTurnedOn = true;
 	}
 
 	@Override
 	public void turnClockOff() {
-		throw new UnsupportedOperationException("Yet to implement");
+		this.isTurnedOn = false;
+	}
+
+	@Override
+	public String getClockDisplayVal(Time time) {
+
+		if (null != time) {
+			if (TimeUtil.isMidnight(time)) { 
+				return (this.isTurnedOn ? "Y-RRRR-RRRR-OOOOOOOOOOO-OOOO" :
+					"O-OOOO-OOOO-OOOOOOOOOOO-OOOO");
+			} else {
+				
+				this.secondsLight = (this.isTurnedOn && time.getSecond()%2 == 0) ? Light.YELLOW.toString() : Light.OFF.toString();
+				
+				IntStream.range(1, 5)
+				.forEach(i -> {
+					this.firstRow = this.firstRow + ((this.isTurnedOn && i <= time.getHour()/5) ? 
+							Light.RED.toString() : Light.OFF.toString());
+					this.secondRow = this.secondRow + ((this.isTurnedOn && i <= time.getHour()%5) ? 
+							Light.RED.toString() : Light.OFF.toString());
+					this.fourthRow = this.fourthRow + ((this.isTurnedOn && i <= time.getMinute()%5) ? 
+							Light.YELLOW.toString() : Light.OFF.toString());
+				});
+				
+				IntStream.range(1, 12)
+				.forEach(i -> {
+					this.thirdRow = this.thirdRow + ((this.isTurnedOn && i <= time.getMinute()/5) ? 
+							((i%3 > 0) ? Light.YELLOW.toString() : Light.RED.toString()) : Light.OFF.toString());
+				});
+				
+			}
+		}
+		
+		return (this.secondsLight + SEPARATOR_HYPHEN + 
+				this.firstRow + SEPARATOR_HYPHEN + 
+				this.secondRow + SEPARATOR_HYPHEN + 
+				this.thirdRow + SEPARATOR_HYPHEN + 
+				this.fourthRow);
 	}
 
 }
--- src/main/java/com/vikhi/practice/clock/Clock.java
+++ src/main/java/com/vikhi/practice/clock/Clock.java
@@ -1,23 +1,13 @@
 package com.vikhi.practice.clock;
 
-import java.util.Date;
+import com.vikhi.practice.pojo.Time;
 
 public interface Clock {
 
-	public int getHour(Date date);
-
-	public int getMinute(Date date);
-	
-	public int getSecond(Date date);
-	
-	public String getHourString(Date date);
-	
-	public String getMinuteString(Date date);
-	
-	public String getSecondString(Date date);
-	
 	public void turnClockOn();
 	
 	public void turnClockOff();
+	
+	public String getClockDisplayVal(Time time);
 	
 }
