c4598b54a251bb794c8c56e417d1752c25ee9bff
Vignesh <Vignesh@S510P_VIG_DUR>
2018-08-01 22:15:16 +0530

    Replaced the Date based exception with a Time based one

--- src/main/java/com/vikhi/practice/throwable/InvalidTimeFormatException.java
+++ src/main/java/com/vikhi/practice/throwable/InvalidTimeFormatException.java
@@ -1,27 +1,27 @@
 package com.vikhi.practice.throwable;
 
-public class InvalidDateFormatException extends Exception {
+public class InvalidTimeFormatException extends Exception {
 
 	/** Generated Serial Version UID. */
 	private static final long serialVersionUID = -5966342403945415017L;
 	
-	public InvalidDateFormatException() {
+	public InvalidTimeFormatException() {
 		super();
 	}
 	
-	public InvalidDateFormatException(String str) {
+	public InvalidTimeFormatException(String str) {
 		super(str);
 	}
 	
-	public InvalidDateFormatException(Throwable cause) {
+	public InvalidTimeFormatException(Throwable cause) {
 		super(cause);
 	}
 	
-	public InvalidDateFormatException(String str, Throwable cause) {
+	public InvalidTimeFormatException(String str, Throwable cause) {
 		super(str, cause);
 	}
 	
-	public InvalidDateFormatException(String str, Throwable cause, 
+	public InvalidTimeFormatException(String str, Throwable cause, 
 			boolean enableSuppression, 
 			boolean writableStackTrace) {
 		super(str, cause, enableSuppression, writableStackTrace);
