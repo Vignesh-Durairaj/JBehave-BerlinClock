33bbcdb042d2701515cc53f3ba2744692b8409dd
Vignesh <Vignesh@S510P_VIG_DUR>
2018-08-02 02:04:13 +0530

    Modified Maven goal to call JBehave testing

--- src/test/java/com/vikhi/practice/clock/BerlinClockStory.java
+++ src/test/java/com/vikhi/practice/clock/BerlinClockStory.java
@@ -2,6 +2,7 @@ package com.vikhi.practice.clock;
 
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.junit.JUnitStory;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
@@ -13,6 +14,7 @@ public class BerlinClockStory extends JUnitStory {
 	@Override
 	public Configuration configuration() {
 		return new MostUsefulConfiguration()
+				.useStoryLoader(new LoadFromClasspath(this.getClass()))
 				.useStoryReporterBuilder(new StoryReporterBuilder()
 						.withDefaultFormats()
 						.withFormats(Format.CONSOLE));
